9aea2789fccadc8c2131392ffe7dc47b293a01a6
am hapi <42941386+hapi-dev@users.noreply.github.com>
2021-07-15 16:49:35 +0200

    Add files via upload

---
+++ susel.cpp
@@ -0,0 +1,41 @@
+//UPDATE V0.69 ALPHA
+#include <susel.h>
+//only real susel's got susel.h source..
+
+void susel::initalize() {
+	initalize_okres();
+	initalize_nerwy();
+	initalize_brzuch();
+	initalize_oczy();
+	initalize_(cala_reszta);
+}
+
+void susel::myslenie() {
+	switch(enable_mozg):
+		case someone_ask:
+			response(rand(ok, niewiem, jestem glodna, mam okres, lubie placki, arin jest kox, ok mozemy, kiedy dott, to co kiedy skonczysz milatapa))
+			break;
+		case przypomnialam_sobie_o_tym_ze_jestem_glodna:
+			reponse(choc na mcdonalda)
+			break;
+
+	//THATS ALL BRUH
+}
+
+void susel::spanie() {
+	sleep(until(11:00AM))
+}
+
+void susel::granie() {
+	initalize_zez();
+}
+
+void susel::strach() {
+	initalize_spierdalanie();
+}
+
+void susel::smiech() {
+	initalize_url("https://img1.dmty.pl//uploads/201006/1276283673_by_laast_600.jpg");
+}
+
+//the end
